c7cede9a2db5b977a4a7f5faa1ae17b862c01f82
anjalisharma-04 <anjalisharma14040@gmail.com>
2025-10-03 14:55:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,6 @@ connectDB()
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running at port : ${process.env.PORT || 8000}`);
         });
-    })
     .catch((err) => {
         console.error("Error starting the server:", err);
     });
